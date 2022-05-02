4eec522ba55093095736b9644e5039e648344d69
kraig <kraighebert@gmail.com>
2022-05-02 15:51:18 -0400

    updated JS

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 const WHITECLOCK = document.getElementById('white-clock')
 const BLACKCLOCK = document.getElementById('black-clock')
+let turn = 'white';
+let whiteClock;
+let blackClock;
+let count = 100000;
+
 
 document.addEventListener('keyup', event => {
     if (event.code === 'Space') {
-      startClock();
+      if (turn == 'white') {
+        clearInterval(blackClock)
+        whiteClock = setInterval(startWhiteClock, 1000);
+      }
+      else {
+        clearInterval(whiteClock)
+        blackClock = setInterval(startBlackClock, 1000);
+      } 
     }
   })
 
-function startClock () {
-    let count;
-    var x = setInterval (function() {
-        count = parseInt(WHITECLOCK.innerHTML) - 1;
-        console.log(WHITECLOCK.innerHTML)
-        WHITECLOCK.innerHTML = count.toString();
-    }, 1000);
+function getTimeRemaining(count) {
+  count = Date.parse(count);
+  const seconds = Math.floor( (count/1000) % 60 );
+  const minutes = Math.floor( (count/1000/60) % 60 ); 
+}
+
+function startWhiteClock () {
+  turn = 'black'
+  count = parseInt(WHITECLOCK.innerHTML) - 1;
+  WHITECLOCK.innerHTML = count.toString();
+}
+
+function startBlackClock () {
+  turn = 'white'
+  count = parseInt(BLACKCLOCK.innerHTML) - 1;
+  BLACKCLOCK.innerHTML = count.toString();
 }
