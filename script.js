02f7b5d0d0675890b1b4c5cc456e10f2995c54fb
Kraig Hebert <kraighebert@gmail.com>
2022-05-02 20:31:57 -0400

    timer complete. space will transfer countdown between sides. active timer highlights done.

--- script.js
+++ script.js
@@ -3,36 +3,68 @@ const BLACKCLOCK = document.getElementById('black-clock')
 let turn = 'white';
 let whiteClock;
 let blackClock;
-let count = 100000;
+let count;
+let minutes;
+let seconds;
 
 
 document.addEventListener('keyup', event => {
-    if (event.code === 'Space') {
-      if (turn == 'white') {
-        clearInterval(blackClock)
-        whiteClock = setInterval(startWhiteClock, 1000);
-      }
-      else {
-        clearInterval(whiteClock)
-        blackClock = setInterval(startBlackClock, 1000);
-      } 
-    }
-  })
+  if (event.code === 'Space') {
+    if (turn == 'white') {
+      clearInterval(blackClock)
+      document.getElementById('white').style.borderColor = 'yellow';
+      document.getElementById('black').style.borderColor = 'black';
+      document.getElementById('white-clock').style.textDecoration = 'underline';
+      document.getElementById('black-clock').style.textDecoration = 'none';
 
-function getTimeRemaining(count) {
-  count = Date.parse(count);
-  const seconds = Math.floor( (count/1000) % 60 );
-  const minutes = Math.floor( (count/1000/60) % 60 ); 
-}
+      whiteClock = setInterval(startWhiteClock, 1000);
+    } else {
+        clearInterval(whiteClock)
+        document.getElementById('black').style.borderColor = 'yellow';
+        document.getElementById('white').style.borderColor = 'black';
+        document.getElementById('black-clock').style.textDecoration = 'underline';
+        document.getElementById('white-clock').style.textDecoration = 'none';
+
+        blackClock = setInterval(startBlackClock, 1000);
+    } 
+  }
+})
 
 function startWhiteClock () {
   turn = 'black'
-  count = parseInt(WHITECLOCK.innerHTML) - 1;
-  WHITECLOCK.innerHTML = count.toString();
-}
+  count = WHITECLOCK.innerHTML;
+  [minutes, seconds] = count.split(':');
+  if (parseInt(minutes) == 0 && parseInt(seconds) == 0) {
+    clearInterval(whiteClock);
+    document.getElementById('white').style.borderColor = 'red';
+    WHITECLOCK.innerHTML = '00:00'
+  } else {
+    [minutes, seconds] = setTimer(count);
+  }
+  WHITECLOCK.innerHTML = minutes + ':' + seconds;
+};
 
 function startBlackClock () {
   turn = 'white'
-  count = parseInt(BLACKCLOCK.innerHTML) - 1;
-  BLACKCLOCK.innerHTML = count.toString();
-}
+  count = BLACKCLOCK.innerHTML;
+  [minutes, seconds] = count.split(':');
+  if (parseInt(minutes) == 0 && parseInt(seconds) == 0) {
+    clearInterval(whiteClock);
+    document.getElementById('white').style.borderColor = 'red';
+    BLACKCLOCK.innerHTML = '00:00'
+  } else {
+    [minutes, seconds] = setTimer(count);
+  }
+  BLACKCLOCK.innerHTML = minutes + ':' + seconds;
+};
+
+function setTimer() {
+  if (parseInt(seconds) > 0) {
+    seconds = parseInt(seconds) - 1;
+    return [minutes, seconds.toString().padStart(2, "0")];
+  } else {
+    minutes = parseInt(minutes) - 1;
+    seconds = 59
+    return[minutes.toString().padStart(2, "0"), seconds.toString().padStart(2, "0")]
+  }
+};
