3466c535c1abd07103f620211555b7053e7f28ea
Kraig Hebert <kraighebert@gmail.com>
2022-05-03 22:07:36 -0400

    refractored code

--- script.js
+++ script.js
@@ -1,73 +1,68 @@
 const WHITECLOCK = document.getElementById('white-clock')
 const BLACKCLOCK = document.getElementById('black-clock')
 let turn = 'white';
-let whiteClock;
-let blackClock;
+let whiteClockTimer;
+let blackClockTimer;
 let count;
 let minutes;
 let seconds;
 
 
+// Event Listener on the space bar to swap timers
 document.addEventListener('keyup', event => {
   if (event.code === 'Space') {
     if (turn == 'white') {
-      clearInterval(blackClock)
-      document.getElementById('white-clock').style.borderColor = 'yellow';
-      document.getElementById('black-clock').style.borderColor = 'black';
-      whiteClock = setInterval(startWhiteClock, 1000);
+      clearInterval(blackClockTimer)
+      WHITECLOCK.style.borderColor = 'yellow';
+      BLACKCLOCK.style.borderColor = 'black';
+      turn = "black";
+      whiteClockTimer = setInterval(startWhiteClock, 1000);
     } else {
-        clearInterval(whiteClock)
-        document.getElementById('black-clock').style.borderColor = 'yellow';
-        document.getElementById('white-clock').style.borderColor = 'black';
-        blackClock = setInterval(startBlackClock, 1000);
+        clearInterval(whiteClockTimer)
+        BLACKCLOCK.style.borderColor = 'yellow';
+        WHITECLOCK.style.borderColor = 'black';
+        turn = "white";
+        blackClockTimer = setInterval(startBlackClock, 1000);
     } 
   }
 })
 
-document.getElementById('game-btn').addEventListener("click", function() {
-});
-document.getElementById('reset-btn').addEventListener("click", function() {
-  stopTimers();
-  WHITECLOCK.innerHTML = "02:00";
-  BLACKCLOCK.innerHTML = "02:00";
-  turn = "white";
-  document.getElementById('white-clock').style.borderColor = 'black';
-  document.getElementById('black-clock').style.borderColor = 'black';
-});
-document.getElementById('stop-btn').addEventListener("click", function() {
-  stopTimers();
-});
+//add game button event listeners
+document.getElementById('game-btn').addEventListener("click", setGame);
+document.getElementById('reset-btn').addEventListener("click", resetClocks);
+document.getElementById('stop-btn').addEventListener("click", stopTimers);
+
 
 
 
 function startWhiteClock () {
-  turn = 'black'
   count = WHITECLOCK.innerHTML;
   [minutes, seconds] = count.split(':');
   if (parseInt(minutes) == 0 && parseInt(seconds) == 0) {
-    clearInterval(whiteClock);
-    document.getElementById('white-clock').style.borderColor = 'red';
+    clearInterval(whiteClockTimer);
+    WHITECLOCK.style.borderColor = 'red';
     WHITECLOCK.innerHTML = '00:00'
   } else {
-    [minutes, seconds] = setTimer(count);
+    [minutes, seconds] = setTimer();
+    WHITECLOCK.innerHTML = minutes + ':' + seconds;
   }
-  WHITECLOCK.innerHTML = minutes + ':' + seconds;
 };
 
+// timer countdown that repeats every 1 second
 function startBlackClock () {
-  turn = 'white'
   count = BLACKCLOCK.innerHTML;
   [minutes, seconds] = count.split(':');
   if (parseInt(minutes) == 0 && parseInt(seconds) == 0) {
-    clearInterval(whiteClock);
-    document.getElementById('black-clock').style.borderColor = 'red';
+    clearInterval(blackClockTimer);
+    BLACKCLOCK.style.borderColor = 'red';
     BLACKCLOCK.innerHTML = '00:00'
   } else {
-    [minutes, seconds] = setTimer(count);
+    [minutes, seconds] = setTimer();
+    BLACKCLOCK.innerHTML = minutes + ':' + seconds;
   }
-  BLACKCLOCK.innerHTML = minutes + ':' + seconds;
 };
 
+// takes current time and ticks off 1 second
 function setTimer() {
   if (parseInt(seconds) > 0) {
     seconds = parseInt(seconds) - 1;
@@ -79,7 +74,23 @@ function setTimer() {
   }
 };
 
+//allows user to choose clock timer type
+function setGame() {
+
+};
+
+//resets clock to original start time
+function resetClocks() {
+  stopTimers();
+  WHITECLOCK.innerHTML = "10:00";
+  BLACKCLOCK.innerHTML = "10:00";
+  turn = "white";
+  WHITECLOCK.style.borderColor = 'black';
+  BLACKCLOCK.style.borderColor = 'black';
+};
+
+//pauses both timers
 function stopTimers() {
-  clearInterval(whiteClock);
-  clearInterval(blackClock);
+  clearInterval(whiteClockTimer);
+  clearInterval(blackClockTimer);
 };
