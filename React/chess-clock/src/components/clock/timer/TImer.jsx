61415dd96ac4539499c740eab8b798282f3d346a
Kraig Hebert <kraighebert@gmail.com>
2022-06-08 22:37:09 -0400

    created main components/css

---
+++ React/chess-clock/src/components/clock/timer/TImer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TImer = () => {
+  return <div>TImer</div>;
+};
+
+export default TImer;
---
+++ React/chess-clock/src/components/clock/timerName/TimerName.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TimerName = () => {
+  return <div>TimerName</div>;
+};
+
+export default TimerName;
