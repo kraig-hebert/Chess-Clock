099668d2744ce859cc1688aa043710c2f07dfc48
Kraig Hebert <kraighebert@gmail.com>
2022-06-17 06:30:49 -0400

    fixed formatting

--- React/chess-clock/src/components/clock/halfClock/timer/Timer.jsx
+++ React/chess-clock/src/components/clock/halfClock/timer/Timer.jsx
@@ -6,9 +6,9 @@ const Timer = ({ timer }) => {
   return (
     <div className="timer">
       <p>
-        {`${timer[0].toString().padStart(2, '0')}
-          :
-          ${timer[1].toString().padStart(2, '0')}`}
+        {`${timer[0].toString().padStart(2, '0')}:${timer[1]
+          .toString()
+          .padStart(2, '0')}`}
       </p>
     </div>
   );
